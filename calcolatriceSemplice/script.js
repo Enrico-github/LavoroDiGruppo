7e52ca24a06545280b32e7d9152b995b11cf6210
Marcello Targa <121225428+targamarcello@users.noreply.github.com>
2025-04-22 14:52:49 +0100

    Delete calcolatriceSemplice directory

--- calcolatriceSemplice/script.js
+++
@@ -1,6 +0,0 @@
-function somma() {
-    const a = parseFloat(document.getElementById("num1").value);
-    const b = parseFloat(document.getElementById("num2").value);
-    const risultato = a + b;
-    document.getElementById("risultato").textContent = "Risultato: " + risultato;
-}
