b4b56eb7f755acef26da93896eab16194b6476ab
FabioCucu <122224459+FabioCucu@users.noreply.github.com>
2025-05-06 12:29:05 +0200

    Add files via upload
    Creazione elementi HTML tramite JS

--- Fabio Cucu Mihai/Lez1DOM/script.js
+++ Fabio Cucu Mihai/Lez1DOM/script.js
@@ -15,3 +15,15 @@ pList.forEach(p => p.style.backgroundColor = "yellow");
 pList.forEach(p => p.style.padding = "10px");
 pList.forEach(p => p.style.margin = "50px");
 
+//Creazione di un nuovo elemento
+const newP = document.createElement("p");
+newP.textContent = "Nuovo paragrafo creato con JS";
+newP.style.backgroundColor = "purple";
+const body = document.getElementById("body");
+body[0].appendChild(newP);
+console.log(body);
+
+const newP2 = document.createElement("p");
+newP2.textContent = "Secondo paragrafo creato con JS";
+newP2.style.backgroundColor = "cyan";
+body[0].insertBefore(newP2, body[0].childNodes[0]);
