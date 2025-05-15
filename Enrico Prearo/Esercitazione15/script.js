8cf6630948b36bacafbaac094c238c778a064c31
Enrico Prearo <121225398+Enrico-github@users.noreply.github.com>
2025-05-15 21:58:22 +0200

    Add files via upload

---
+++ Enrico Prearo/Esercitazione15/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    document.getElementById("scroll-bar").style.width = scrollPercent + "%";
+  });
+  
